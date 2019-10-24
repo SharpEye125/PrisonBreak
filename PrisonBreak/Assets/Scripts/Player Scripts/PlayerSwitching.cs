af07c81046496f53f3ffc8e1e4e0175ae9914083
Malachi Weis <21.malachi.weis@ksd.org>
2019-10-24 13:13:50 -0700

    Player Switching script added

--- PrisonBreak/Assets/Scripts/Player Scripts/PlayerSwitching.cs
+++ PrisonBreak/Assets/Scripts/Player Scripts/PlayerSwitching.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerSwitching : MonoBehaviour
 {
+    public Transform Smart;
+    public Transform Brawn;
+    bool smart = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,13 +16,27 @@ public class PlayerSwitching : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         if (Input.GetButtonDown("Space"))
         {
-
+            SwitchPlayers();
         }
     }
     void SwitchPlayers()
     {
-
+        if (smart = true)
+        {
+            
+            transform.position = new Vector3(Brawn.position.x,
+    Brawn.position.y, transform.position.z);
+            smart = false;
+        }
+        else if (smart = false)
+        {
+            transform.position = new Vector3(Smart.position.x,
+    Smart.position.y, transform.position.z);
+            smart = true;
+        }
     }
+
 }
