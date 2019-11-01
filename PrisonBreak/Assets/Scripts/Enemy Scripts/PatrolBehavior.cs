5dda0baf3575f8f73bcfe1f39f263bedbf176253
Thomas Davis <21.thomas.davis@ksd.org>
2019-11-01 13:43:14 -0700

    added pathfinding code in the guard AI scipt diden't rename the the script though

--- PrisonBreak/Assets/Scripts/Enemy Scripts/PatrolBehavior.cs
+++ PrisonBreak/Assets/Scripts/Enemy Scripts/PatrolBehavior.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class PatrolBehavior : MonoBehaviour
 {
@@ -13,10 +14,40 @@ public class PatrolBehavior : MonoBehaviour
     int waypointIndex = 0;
     float waitTime = 3f;
     float timer = 0;
+    public Transform target;
+
+    [SerializeField]
+    public float chaseSpeed = 9f;
+
+    public float nextWaypointDistance = 3f;
+    Path path;
+    int currentWaypoint = 0;
+    bool reachedEndOfPath = false;
+    Seeker seeker;
+    Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = waypoints[waypointIndex].transform.position; 
+        transform.position = waypoints[waypointIndex].transform.position;
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+        InvokeRepeating("UpdatePath", 0f, .5f);
+        seeker.StartPath(rb.position, target.position, OnPathComplete);
+    }
+    void UpdatePath()
+    {
+        if (seeker.IsDone())
+        {
+            seeker.StartPath(rb.position, target.position, OnPathComplete);
+        }
+    }
+    void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
     }
 
     // Update is called once per frame
@@ -26,13 +57,37 @@ public class PatrolBehavior : MonoBehaviour
         if(timer > waitTime)
         {
             Move ();
+            if (path == null)
+            {
+                return;
+            }
+            if (currentWaypoint >= path.vectorPath.Count)
+            {
+                reachedEndOfPath = true;
+                return;
+            }
+            else
+            {
+                reachedEndOfPath = false;
+            }
+
+            Vector2 direction = ((vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+            Vector2 force = direction * chaseSpeed * Time.deltaTime;
+
+            rb.AddForce(force);
+
+            float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+            if (distance < nextWaypointDistance)
+            {
+                currentWaypoint++;
+            }
 
         }
     }
 
     void Move()
     {
-        transform.position = Vector2.MoveTowards(transform.position, waypoints[waypointIndex].transform.position, moveSpeed * Time.deltaTime);
+        //transform.position = Vector2.MoveTowards(transform.position, waypoints[waypointIndex].transform.position, moveSpeed * Time.deltaTime);
         if (transform.position == waypoints[waypointIndex].transform.position)
         {
             waypointIndex += 1;
