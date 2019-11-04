b9d74a473ffbade6225e559b1e4633c3851b70d5
Malachi Weis <21.malachi.weis@ksd.org>
2019-11-04 14:00:19 -0800

    Minor changes to PUAT script

--- PrisonBreak/Assets/Scripts/PopUpActionText.cs
+++ PrisonBreak/Assets/Scripts/PopUpActionText.cs
@@ -7,11 +7,11 @@ public class PopUpActionText : MonoBehaviour
 {
     public Transform Camera;
     public float showUpDistance = 5.0f;
-    public text Text;
+    public Text Text;
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Canvas>().enabled = false;
+
     }
 
     // Update is called once per frame
@@ -21,11 +21,11 @@ public class PopUpActionText : MonoBehaviour
                    Camera.gameObject.GetComponent<PlayerSwitching>().target.position.y - transform.position.y);
         if (playerDir.magnitude >= showUpDistance)
         {
-            GetComponent<Canvas>().enabled = true;
+            
         }
         else
         {
-            GetComponent<Canvas>().enabled = false;
+            
         }
     }
 }
